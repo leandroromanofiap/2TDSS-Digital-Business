5c56dc37fcb8567d8dd2c3ff83a18880089fbf57
Leandro Romano <rm75244@fiap.com.br>
2018-03-14 22:15:27 -0300

    updates

--- 07-WS-Restful-Server/src/br/com/fiap/resource/ProdutoResource.java
+++ 07-WS-Restful-Server/src/br/com/fiap/resource/ProdutoResource.java
@@ -1,16 +1,24 @@
 package br.com.fiap.resource;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
 
 import br.com.fiap.jpa.dao.ProdutoDAO;
 import br.com.fiap.jpa.dao.impl.ProdutoDAOImpl;
+import br.com.fiap.jpa.exception.CommitException;
 import br.com.fiap.jpa.singleton.EntityManagerFactorySingleton;
 import br.com.fiap.to.ProdutoTO;
 
@@ -26,15 +34,25 @@ public class ProdutoResource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<ProdutoTO> buscar() {
+	public List<ProdutoTO> listar() {
+		return produtoDao.listar();
+	}
 
-		List<ProdutoTO> produtos = new ArrayList<>();
-
-		produtos.add(new ProdutoTO(1, "Produto1", "Descricao1", 99, true));
-		produtos.add(new ProdutoTO(2, "Produto2", "Descricao2", 49, false));
-
-		return produtos;
-
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response cadastrar(ProdutoTO produto, @Context UriInfo uriInfo) {
+		try {
+			produtoDao.inserir(produto);
+			produtoDao.commit();
+			
+			UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+			builder.path(Integer.toString(produto.getCodigo()));
+			
+			return Response.created(builder.build()).build();
+		} catch (CommitException e) {
+			e.printStackTrace();
+			throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
