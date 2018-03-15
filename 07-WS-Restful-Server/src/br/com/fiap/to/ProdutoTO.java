f42450e2221a0cf13a8bffc67fa7efeeb07dddd9
Leandro Romano <rm75244@fiap.com.br>
2018-03-14 21:38:42 -0300

    Mapeado ProdutoTO JPA

--- 07-WS-Restful-Server/src/br/com/fiap/to/ProdutoTO.java
+++ 07-WS-Restful-Server/src/br/com/fiap/to/ProdutoTO.java
@@ -2,23 +2,34 @@ package br.com.fiap.to;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
+@Entity
+@Table(name = "T_PRODUTO")
+@SequenceGenerator(name = "produto", sequenceName = "SQ_T_PRODUTO", allocationSize = 1)
 public class ProdutoTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(generator = "produto", strategy = GenerationType.SEQUENCE)
 	private int codigo;
-	
+
 	private String nome;
-	
+
 	private String descricao;
-	
+
 	private double preco;
-	
+
 	private boolean disponivel;
-	
+
 	public ProdutoTO() {
 		super();
 	}
@@ -71,5 +82,5 @@ public class ProdutoTO implements Serializable {
 	public void setDisponivel(boolean disponivel) {
 		this.disponivel = disponivel;
 	}
-	
+
 }
